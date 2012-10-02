3f209dc503d74660fbc230eac7f7d6fa1966d53a
Blithe Rocher <blitherocher@gmail.com>
2012-10-01 21:40:07 -0400

    Added authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Blogger::Application.routes.draw do
+  resources :authors, :articles, :comments, :tags, :author_sessions
+
   get "tags/index"
 
   get "tags/show"
 
-  resources :articles 
-  resources :comments
-  resources :tags
+  match 'login' => 'author_sessions#new',		:as => :login
+  match 'logout' => 'author_sessions#destroy',	:as => :logout
+
+  root :to => 'articles#index'
 end
